328ec2d03dfbba926ca490e3b8185430259a340f
TomIshiyama <honngannji@gmail.com>
2024-02-25 23:48:47 +0900

    :sparkles: feat: create setting file for `md-to-pdf`

---
+++ pdf-configs/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stylesheet: "./pdf-configs/style.css",
+  body_class: "markdown-body",
+  marked_options: {
+    headerIds: false,
+    smartypants: true,
+  },
+  pdf_options: {
+    format: "A4",
+    margin: "30mm 20mm",
+    printBackground: true,
+    headerTemplate:
+      "<style>\n  section {\n    margin: 0 auto;\n    font-size: 9px;\n  }\n</style>",
+    footerTemplate:
+      '<section>\n  <div>\n    <span class="page-number"></span>\n    / <span class="total-pages"></span>\n  </div>\n</section>',
+  },
+  stylesheet_encoding: "utf-8",
+};
